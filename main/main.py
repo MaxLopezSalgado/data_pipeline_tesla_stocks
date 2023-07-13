1a01a2a87323b7b413ec9695ecec33b0a02b8a90
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-07-13 18:33:17 +0200

    linked the script with the token through the text file

--- main/main.py
+++ main/main.py
@@ -1,7 +1,13 @@
 import urllib3
 import json
+
+
 def get_data():
     http = urllib3.PoolManager()
+    # Read token from file
+    with open("token.txt", "r") as file:
+        token = file.read().strip()
+        
     url = f"https://cloud.iexapis.com/stable/stock/tsla/previous?token={token}"
     resp = http.request("GET", url)
     values = json.loads(resp.data)
