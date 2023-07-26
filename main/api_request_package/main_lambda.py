8a2b8c89679ac98678f0a07b4e6bae0e09764050
MaxLopezSalgado <maxlopezsalgado@gmail.com>
2023-07-26 14:59:29 +0200

    updated needed files for the deployment

--- main/api_request_package/main_lambda.py
+++ main/api_request_package/main_lambda.py
@@ -2,12 +2,7 @@ import ssl
 import urllib3
 import json
 import os
-import streamlit as lt
 
-from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.getenv("stocks_key")
 def get_data_lambda(event, context):
     # Print the contents of /tmp/ to check if the file is present
     print(os.listdir("/tmp/"))
@@ -16,7 +11,12 @@ def get_data_lambda(event, context):
     ssl_context.verify_mode = ssl.CERT_NONE
 
     http = urllib3.PoolManager(cert_reqs='CERT_NONE', ssl_context=ssl_context)
-    url = f"https://cloud.iexapis.com/stable/stock/tsla/previous?token={api_key}"
+
+    # Use the absolute path for the token file
+    with open("token.txt", "r") as file:
+        token = file.read().strip()
+
+    url = f"https://cloud.iexapis.com/stable/stock/tsla/previous?token={token}"
     resp = http.request("GET", url)
     values = json.loads(resp.data)
     return values
